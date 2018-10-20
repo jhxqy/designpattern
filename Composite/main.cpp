a18b26064f1e006b1794b79855396de12e5d4641
jhxqy <1139648416@qq.com>
2018-10-20 18:27:14 +0800

    Composite

--- Composite/main.cpp
+++ Composite/main.cpp
@@ -10,8 +10,20 @@
 #include "Composite.hpp"
 
 int main(){
-    Leaf *j=new Leaf("jhx");
-    Leaf *z=new Leaf("zmm");
-    Leaf *x=new Leaf("xqw");
-    
+    Leaf *jhx=new Leaf("jhx");
+    Leaf *zmm=new Leaf("zmm");
+    Leaf *xqw=new Leaf("xqw");
+    Leaf *qjw=new Leaf("qjw");
+    Leaf *zxl=new Leaf("zxl");
+    Composite *c1=new Composite("网络171");
+    c1->Add(qjw);
+    c1->Add(xqw);
+    Composite *c2=new Composite("网络172");
+    c2->Add(jhx);
+    c2->Add(zmm);
+    Composite *wl=new Composite("网络工程");
+    wl->Add(zxl);
+    wl->Add(c1);
+    wl->Add(c2);
+    wl->Operator(0);
 }
