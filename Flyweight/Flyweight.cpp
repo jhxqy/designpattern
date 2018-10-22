b186d2f5b5ba7801d988bcd47214ef45f2986915
jhxqy <1139648416@qq.com>
2018-10-22 19:47:16 +0800

    Flyweight

---
+++ Flyweight/Flyweight.cpp
@@ -0,0 +1,9 @@
+//
+//  Flyweight.cpp
+//  DesignPattern
+//
+//  Created by 贾皓翔 on 2018/10/22.
+//  Copyright © 2018 贾皓翔. All rights reserved.
+//
+
+#include "Flyweight.hpp"
---
+++ Flyweight/Flyweight.hpp
@@ -0,0 +1,14 @@
+//
+//  Flyweight.hpp
+//  DesignPattern
+//
+//  Created by 贾皓翔 on 2018/10/22.
+//  Copyright © 2018 贾皓翔. All rights reserved.
+//
+
+#ifndef Flyweight_hpp
+#define Flyweight_hpp
+
+#include <stdio.h>
+
+#endif /* Flyweight_hpp */
